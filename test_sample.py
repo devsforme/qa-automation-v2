222b80f0c449364197b5b8d399254f597b0dc090
devsforme <jonnyfor1000@gmail.com>
2025-10-06 21:10:24 +0200

    Update test_sample.py

--- test_sample.py
+++ test_sample.py
@@ -1,5 +1,13 @@
 def test_sum():
     assert 2 + 3 == 5
+print("=== Запуск тестов начался ===")
+# твои тесты
+
+def test_example():
+    print("Тест test_example запущен")
+    assert 1 + 1 == 2
+
+print("=== Запуск тестов завершён ===")
 
 def test_upper():
     assert "hello".upper() == "HELLO"
